b99857b20b38f5eca3012eb1e32452edc3dab813
김주호/기타 <jhkim@humanworks.com>
2017-05-29 15:25:05 +0900

    ''

--- index.js
+++ index.js
@@ -11,6 +11,37 @@ app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+/*
+ * Verify that the callback came from Facebook. Using the App Secret from
+ * the App Dashboard, we can verify the signature that is sent with each
+ * callback in the x-hub-signature field, located in the header.
+ *
+ * https://developers.facebook.com/docs/graph-api/webhooks#setup
+ *
+ */
+function verifyRequestSignature(req, res, buf) {
+    var signature = req.headers["x-hub-signature"];
+
+    if (!signature) {
+        // For testing, let's log an error. In production, you should throw an
+        // error.
+        console.error("Couldn't validate the signature.");
+    } else {
+        var elements = signature.split('=');
+        var method = elements[0];
+        var signatureHash = elements[1];
+
+        var expectedHash = crypto.createHmac('sha1', 'd5768f1da0c3ebc6b1fb286b7523caff')
+            .update(buf)
+            .digest('hex');
+
+        if (signatureHash != expectedHash) {
+            throw new Error("Couldn't validate the request signature.");
+        }
+    }
+}
+
 app.use(bodyParser.json({ verify: verifyRequestSignature }));
 
 app.get('/', function(request, response) {
@@ -107,36 +138,6 @@ function sendTextMessage(recipientId, messageText) {
     };
 
     callSendAPI(messageData);
-}
-
-/*
- * Verify that the callback came from Facebook. Using the App Secret from
- * the App Dashboard, we can verify the signature that is sent with each
- * callback in the x-hub-signature field, located in the header.
- *
- * https://developers.facebook.com/docs/graph-api/webhooks#setup
- *
- */
-function verifyRequestSignature(req, res, buf) {
-    var signature = req.headers["x-hub-signature"];
-
-    if (!signature) {
-        // For testing, let's log an error. In production, you should throw an
-        // error.
-        console.error("Couldn't validate the signature.");
-    } else {
-        var elements = signature.split('=');
-        var method = elements[0];
-        var signatureHash = elements[1];
-
-        var expectedHash = crypto.createHmac('sha1', 'd5768f1da0c3ebc6b1fb286b7523caff')
-            .update(buf)
-            .digest('hex');
-
-        if (signatureHash != expectedHash) {
-            throw new Error("Couldn't validate the request signature.");
-        }
-    }
 }
 
 function callSendAPI(messageData) {
