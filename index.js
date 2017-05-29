c1e97d90ad20e1ddf3090f1091a90b40258c7e65
김주호/기타 <jhkim@humanworks.com>
2017-05-29 14:48:26 +0900

    ''

--- index.js
+++ index.js
@@ -25,34 +25,33 @@ app.get('/webhook', function(req, res) {
 });
 
 app.post('/webhook', function (req, res) {
-    var data = req.body;
+    console.log('>>> ' + JSON.stringify(req));
+    if(req.body){
+        // Make sure this is a page subscription
+        if (data.object === 'page') {
 
-    console.log('>>> ' + JSON.stringify(data));
+            // Iterate over each entry - there may be multiple if batched
+            data.entry.forEach(function(entry) {
+                var pageID = entry.id;
+                var timeOfEvent = entry.time;
 
-    // Make sure this is a page subscription
-    if (data.object === 'page') {
-
-        // Iterate over each entry - there may be multiple if batched
-        data.entry.forEach(function(entry) {
-            var pageID = entry.id;
-            var timeOfEvent = entry.time;
-
-            // Iterate over each messaging event
-            entry.messaging.forEach(function(event) {
-                if (event.message) {
-                    receivedMessage(event);
-                } else {
-                    console.log("Webhook received unknown event: ", event);
-                }
+                // Iterate over each messaging event
+                entry.messaging.forEach(function(event) {
+                    if (event.message) {
+                        receivedMessage(event);
+                    } else {
+                        console.log("Webhook received unknown event: ", event);
+                    }
+                });
             });
-        });
 
-        // Assume all went well.
-        //
-        // You must send back a 200, within 20 seconds, to let us know
-        // you've successfully received the callback. Otherwise, the request
-        // will time out and we will keep trying to resend.
-        res.sendStatus(200);
+            // Assume all went well.
+            //
+            // You must send back a 200, within 20 seconds, to let us know
+            // you've successfully received the callback. Otherwise, the request
+            // will time out and we will keep trying to resend.
+            res.sendStatus(200);
+        }
     }
 });
 
