4bcf17728c63213a3d11c2b3b9b070bf7e4120c4
김주호/기타 <jhkim@humanworks.com>
2017-05-29 15:23:14 +0900

    ''

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.get('/webhook', function(req, res) {
 app.post('/webhook', function (req, res) {
     console.log('----> POST Webhook');
     if(req.body){
+        var data = req.body;
         // Make sure this is a page subscription
         if (data.object === 'page') {
 
