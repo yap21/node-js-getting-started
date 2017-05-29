59067def9f42d2838dd9ee8fcb6196f670ca2efa
김주호/기타 <jhkim@humanworks.com>
2017-05-29 14:22:25 +0900

    no message

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-'use strict'
-
-// recommended to inject access tokens as environmental variables, e.g.
-// const token = process.env.FB_PAGE_ACCESS_TOKEN
-const token = "EAAGWH5Hh7IMBAB2xI3a9D9d8PGNzFSzZAQnN72Na3e7X2u5L8X1zkJKnI1kP9fi1FscHquxVfVbgypXRmygFUIAbAnAt55GoLckUYaP8ZAhV6LZC93TfwNNnCKJ6AhbZBdkpN0B14SJyXUdXSRT9jFeg2RSpAVKF8uqy9ZCd9YQZDZD"
-const verify_token = 'boing';
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
-const app = express();
+var express = require('express');
+var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -19,11 +10,10 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+    response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
-
 
